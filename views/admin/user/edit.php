19b8f203b93b20e5bfd07d5f0b35c908e4928f98
JoaoErick <jerick1415@outlook.com>
2020-09-29 20:52:18 -0300

    Add javascript function to validate password

--- views/admin/user/edit.php
+++ views/admin/user/edit.php
@@ -9,10 +9,10 @@
 
 
 <html>
-    <form action="/Treinamento2020/user/update/<?php echo $user->getId()?>" method="post">
-        <input name="name" placeholder="name" value="<?php echo $user->getName()?>">
-        <input type="email" name="email" placeholder="email" value="<?php echo $user->getEmail()?>">
-        <select name="type">
+    <form action="/Treinamento2020/user/update/<?php echo $user->getId()?>" method="post" onsubmit="return validarSenha()" name="form">
+        <input name="name" placeholder="name" value="<?php echo $user->getName()?>" required>
+        <input type="email" name="email" placeholder="email" value="<?php echo $user->getEmail()?>" required>
+        <select name="type" required>
             <option value="">Selecione um tipo</option>
             <option value="admin"<?php if($user->getType() == "admin"){?> selected <?php }?>>Administrador</option>
             <option value="user" <?php if($user->getType() == "user"){?> selected <?php }?>>Usuário comum</option>
@@ -21,4 +21,17 @@
         <input type="password" name="password_confirmation" placeholder="Confirme a senha">
         <button type="submit"> Editar </button>        
     </form>
+    <script>
+        function validarSenha(){
+            senha = document.form.password.value;
+            confirma_senha = document.form.password_confirmation.value;
+            if (senha == confirma_senha){
+                return true;
+            }
+            else{ 
+                alert("As senhas não são compatíveis!");
+                return false;
+            }
+        }
+    </script>
 </html>
